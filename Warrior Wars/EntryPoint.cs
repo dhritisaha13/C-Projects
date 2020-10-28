f745255dbf4bef3e2ff6def59cc9a7bb1d877fd3
Dhriti Saha <39408900+dhritisaha13@users.noreply.github.com>
2020-10-28 21:21:04 +0530

    Add files via upload

---
+++ Warrior Wars/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace WarriorWars
+{
+    class EntryPoint
+    {
+        static Random rng = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Dhriti", Enum.Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Riya", Enum.Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rng.Next(0, 10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                    
+                }
+                Thread.Sleep(100);
+            }
+
+
+
+
+           
+        }
+    }
+}
